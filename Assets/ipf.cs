9a8a63d264c00c09479a236b2cfb91a9ec459c5b
Bjeck <bjarke@larsen-dk.dk>
2017-06-05 12:58:56 +0200

    basic project

---
+++ Assets/ipf.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ipf : MonoBehaviour {
+
+    [SerializeField] GlitchManager glitchM;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TriggerGlitch()
+    {
+        glitchM.GlitchScreenOnCommand(0.1f, 2f);
+        
+    }
+
+}
---
+++ Assets/trainmovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trainmovement : MonoBehaviour {
+
+    public float amplitude = 1;
+    public float freq = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0, Mathf.Sin(Time.time * freq) * amplitude, 0);
+
+    }
+}
